3c0a609a2c5c7d25297474b62cfa99ee828106b8
hendriyanhasby97-hash <hendriyanhasby.97@gmail.com>
2026-06-05 11:28:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 // 1. KONFIGURASI SUPABASE
 // ==========================================
 // Pastikan URL dan Anon Key ini sudah benar milikmu
-const SUPABASE_URL = 'https://PROYEK_KAMU.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJI...'; 
+const SUPABASE_URL = 'https://rjdymyzeujfxzqwwhxbb.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqZHlteXpldWpmeHpxd3doeGJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MjI2OTQsImV4cCI6MjA5NjE5ODY5NH0.dUvvgTawV0ZoOGy8VFJ91GkxNufqcITRBAiSoQ3q3tc'; 
 
 // PERBAIKAN: Ubah nama variabel menjadi supabaseClient agar tidak bentrok
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
